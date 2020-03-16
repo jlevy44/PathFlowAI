5dee2399521068faf08e17716d25dd800f398823
sumanthratna <sumanthratna@gmail.com>
2020-03-15 23:16:00 -0400

    Programmatically download test file

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,11 +1,20 @@
-from pathflowai import utils
-import numpy as np
-from os import path
-
-
 def test_svs2dask_array():
-    tests_dir = path.dirname(path.realpath(__file__))
-    ground_truth = np.load(path.join(tests_dir, 'sample.npy'))
-    test = utils.svs2dask_array(path.join(tests_dir, 'sample.tif')).compute()
-    assert np.allclose(ground_truth, test)
-    # assert np.array_equal(ground_truth, test)
+    from pathflowai import utils
+    from .utils import download_svs
+    from PIL import Image
+    import numpy as np
+    from os import remove
+
+    id = "2e4f6316-588b-4629-adf0-7aeac358a0e2"
+    file = "TCGA-MR-A520-01Z-00-DX1.2F323BAC-56C9-4A0C-9C1B-2B4F776056B4.svs"
+    download_location = download_svs(id, file)
+
+    Image.MAX_IMAGE_PIXELS = None  # SECURITY RISK!
+    ground_truth = np.array(Image.open(download_location))
+
+    test = utils.svs2dask_array(download_location).compute()
+    crop_height, crop_width, _ = test.shape
+
+    remove(download_location)
+
+    assert np.array_equal(ground_truth[:crop_height, :crop_width, :], test)
---
+++ tests/utils.py
@@ -0,0 +1,25 @@
+def download_svs(id, filename):
+    from os import path, remove
+    import requests
+
+    url = f"https://api.gdc.cancer.gov/data/{id}"
+    print(f"Downloding from {url}")
+
+    tests_dir = path.dirname(path.realpath(__file__))
+    download_location = f"{tests_dir}/{filename}"
+
+    try:
+        remove(download_location)
+    except OSError:
+        pass
+
+    r = requests.get(url, allow_redirects=True)
+    open(download_location, 'wb').write(r.content)
+    # with requests.get(url, allow_redirects=True, stream=True) as r:
+    #     with open(download_location, 'wb') as f:
+    #         for chunk in r.iter_content():
+    #             if chunk:  # filter out keep-alive new chunks
+    #                 f.write(chunk)
+    #                 f.flush()
+
+    return download_location
