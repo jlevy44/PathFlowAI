4869050e89f842b796f1eb787192a1b049fd8cb5
sumanthratna <sumanthratna@gmail.com>
2020-03-20 10:49:31 -0400

    Add Travis CI

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,7 +3,7 @@ def test_svs2dask_array():
     from .utils import download_svs
     from PIL import Image
     import numpy as np
-    from os import remove
+    # from os import remove
 
     id = "2e4f6316-588b-4629-adf0-7aeac358a0e2"
     file = "TCGA-MR-A520-01Z-00-DX1.2F323BAC-56C9-4A0C-9C1B-2B4F776056B4.svs"
@@ -15,6 +15,6 @@ def test_svs2dask_array():
     test = utils.svs2dask_array(download_location).compute()
     crop_height, crop_width, _ = test.shape
 
-    remove(download_location)
+    # remove(download_location)
 
     assert np.array_equal(ground_truth[:crop_height, :crop_width, :], test)
