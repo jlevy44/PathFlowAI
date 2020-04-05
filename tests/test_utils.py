762eef75c496dee26ad2891dd4deb1bcfbc20615
sumanthratna <sumanthratna@gmail.com>
2020-04-04 23:04:52 -0400

    Update dependencies

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,26 +2,26 @@ from pathflowai import utils
 from numpy import array_equal
 
 
-# def test_svs2dask_array():
-#     from .utils import download_svs
-#     from PIL import Image
-#     from numpy import array as to_npa
-#
-#     # from os import remove
-#
-#     id = "2e4f6316-588b-4629-adf0-7aeac358a0e2"
-#     file = "TCGA-MR-A520-01Z-00-DX1.2F323BAC-56C9-4A0C-9C1B-2B4F776056B4.svs"
-#     download_location = download_svs(id, file)
-#
-#     Image.MAX_IMAGE_PIXELS = None  # SECURITY RISK!
-#     ground_truth = to_npa(Image.open(download_location))
-#
-#     test = utils.svs2dask_array(download_location).compute()
-#     crop_height, crop_width, _ = test.shape
-#
-#     # remove(download_location)
-#
-#     assert array_equal(ground_truth[:crop_height, :crop_width, :], test)
+def test_svs2dask_array():
+    from .utils import download_svs
+    from PIL import Image
+    from numpy import array as to_npa
+
+    # from os import remove
+
+    id = "2e4f6316-588b-4629-adf0-7aeac358a0e2"
+    file = "TCGA-MR-A520-01Z-00-DX1.2F323BAC-56C9-4A0C-9C1B-2B4F776056B4.svs"
+    download_location = download_svs(id, file)
+
+    Image.MAX_IMAGE_PIXELS = None  # SECURITY RISK!
+    ground_truth = to_npa(Image.open(download_location))
+
+    test = utils.svs2dask_array(download_location).compute()
+    crop_height, crop_width, _ = test.shape
+
+    # remove(download_location)
+
+    assert array_equal(ground_truth[:crop_height, :crop_width, :], test)
 
 
 def test_preprocessing_pipeline():
@@ -97,6 +97,7 @@ def test_preprocessing_pipeline():
             "-t", "0.05"
         ]
         out, err, exitcode = capture(command)
+        print(err)
         assert exists(out_zarr)
         assert exists(out_pkl)
         assert exists(odb)
